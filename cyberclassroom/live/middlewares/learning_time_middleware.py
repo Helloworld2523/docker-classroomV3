5b82a07fbac5c6df215c796964a7e527a93a08ab
helloworld2523 <helloworld2523@gmail.com>
2026-04-09 15:52:17 +0700

    ปรบหน้าจอเล่น และ ปับหน้าจอ การแจ้งเวลา หาก หมดเวลาสอนทุกวิชา

--- cyberclassroom/live/middlewares/learning_time_middleware.py
+++ cyberclassroom/live/middlewares/learning_time_middleware.py
@@ -20,7 +20,7 @@ class LearningHoursMiddleware:
 
         now = datetime.now().time()
         start_time = time(7, 0)   # แก้ไขจาก 7:10 เป็น 7:00
-        end_time = time(20, 30)   # เวลา 20:30 ตามที่ต้องการ
+        end_time = time(14, 30)   # เวลา 20:30 ตามที่ต้องการ
         if not (start_time <= now <= end_time):
             return render(request, 'live/out_of_learning_time.html', {
                 'start_time': start_time.strftime('%H:%M'),
