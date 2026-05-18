398ee3af4fafa50be70139ad5e0f40a93c13f694
helloworld2523 <helloworld2523@gmail.com>
2026-05-18 08:59:51 +0700

    ???

--- backupV1/cyberclassroom/live/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import ClassScheduleCenter
-
-class ClassScheduleCenterSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ClassScheduleCenter
-        fields = '__all__'
